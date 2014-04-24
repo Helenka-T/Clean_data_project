b4e8e10fc7c947802d81fc13eee65b41ecb0b340
Helenka-T <tikhonova.elena@yahoo.com>
2014-04-24 23:32:52 +0200

    Stuff.

--- run_analysis.R
+++ run_analysis.R
@@ -1,53 +1,68 @@
+#Course project
 
-##Download test and train data for the analysis
+# Read data
+activity.names <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/activity_labels.txt")[,2]
+test.data <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/test/X_test.txt")
+train.data <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/train/X_train.txt")
 
-x_test <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/test/X_test.txt")
-x_train <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/train/X_train.txt")
-y_test <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/test/y_test.txt")
-y_train <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/train/y_train.txt")
+test.activity <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/test/y_test.txt")[,1]
+train.activity <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/train/y_train.txt")[,1]
+
+test.subject <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/test/subject_test.txt")[,1]
+train.subject <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/train/subject_train.txt")[,1]
+
+interesting.features <- grep("-mean\\(\\)|-std\\(\\)", feature.names)
+x_test <- x_test[,interesting.features]
+x_train <- x_train[,interesting.features]
+
+x_test$activity <- activity.names[y_test]
+x_train$activity <- activity.names[y_train]
 
 #Item1: Merging test and train data
-
-x_test_train = data.frame(mapply(append, x_test, x_train))
-y_test_train = data.frame(mapply(append, y_test, y_train))
+all.data <- data.frame(mapply(append, test.data, train.data))
+all.activity <- append(test.activity, train.activity)
+all.subject <- append(test.subject, train.subject)
+subject_test_train = data.frame(mapply(append, subject_test, subject_train))
 
 #Item2: Extracting the mean and standard deviation for all measurment 
 ##Creating function to calculate mean values for x
-mean_data <- function (data, id = 1:561){
-        p <- c()
-        for (i in id){
-                p <- c(p, mean(data[,i]))
-        }
-        p
+feature.names <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/features.txt")[,2]
+interesting.features <- grep("-mean\\(\\)|-std\\(\\)", feature.names)
+all.data <- all.data[,interesting.features]
+feature.names <- feature.names[interesting.features]
+
+#Item3: Descriptive activity names
+activity.names <- read.table("C:/Users/Elena/Desktop/UCI HAR Dataset/activity_labels.txt")[,2]
+activity.names <- gsub("_", " ", tolower(as.character(activity.names)))
+all.data$activity <- as.factor(activity.names[all.activity])
+
+#Item4: Labels in the data set
+# Replace - by . and delete parantheses
+feature.names <- gsub("-", ".", gsub("[()]", "", feature.names))
+colnames(all.data) <- append(feature.names, c("activity"))
+
+write.table(all.data, "C:/Users/Elena/Desktop/tidy_data.txt")
+
+
+# Item 5: Creating the 2nd tidy data set
+per.subject <- split(all.data, all.subject)
+names(per.subject[[1]])
+averagePerActivity <- function(frame) {
+        per.activity <- split(frame, frame$activity)
+        means <- lapply(per.activity, function(x) mapply(mean, x[-ncol(x)]))
+        result <- cbind(do.call(rbind, means), data.frame(activity=names(means)))
+        rownames(result) <- NULL
+        result
 }
-
-##Calculating mean values for x
-mean_x = mean_data(x_test_train)
-##Calculating mean values for y
-mean_y = sapply(y_test_train, mean)
-
-##Creating a function to calculate standard deviation for x
-st_dev <- function (data, id = 1:561){
-        p <- c()
-        for (i in id){
-                p <- c(p, sd(data[,i]))
+tidy <- NA
+for (i in 1:30) {
+        subject <- averagePerActivity(per.subject[[i]])
+        subject$subject <- rep(i, nrow(subject))
+        if (is.na(tidy)) {
+                tidy <- subject
+        } else {
+                tidy <- rbind(tidy, subject)
         }
-        p
 }
-
-##Calculating standard deviation for x
-std_x = st_dev(x_test_train)
-##Calculating standard deviation for y
-std_y = sapply(y_test_train, sd)
-
-x_all <- data.frame(mapply(append, x_test_train, mean_x))
-x_all <- data.frame(mapply(append, x_all, std_x))
-y_all <- data.frame(mapply(append, y_test_train, mean_y))
-y_all <- data.frame(mapply(append, y_all, std_y))
-
-rownames(x_all)[10300] <- "Mean values"
-rownames(x_all)[10301] <- "St. dev."
-
-rownames(y_all)[10300] <- "Mean values"
-rownames(y_all)[10301] <- "St. dev"
-
+# Put subject and activity to the front
+tidy1 <- tidy[,c(ncol(tidy), ncol(tidy)-1, seq(1,ncol(tidy)-2))]
